8fc93719078e51f80b9bf3dcd751f218aed7dac9
shekinah <nyamburamary1212@gmail.com>
2024-01-23 10:54:37 +0300

    Debug

--- server/app.py
+++ server/app.py
@@ -1,42 +1,29 @@
 from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-from flask_swagger_ui import get_swaggerui_blueprint
 from models import db, User, Plan, Instructor
 
-app = Flask(__name__)
+app =Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False  # Set this to False to disable pretty printing
+app.config['SQLALCHEMY_TRACK_MODOFICATIONS'] = False
+app.json.compact = False
+
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
-api = Api(app)
 
-# Create Swagger UI Blueprint
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Power'f GYM API"
-    }
-)
+@app.route('/')
+def home():
+    return "<h1>Welcome to Power'f GYM</h1>"
 
-app.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
-
-# Create Flask-RESTful API
-api = Api(app)
-
-# GET /users
-class UsersEndpoint(Resource):
-    def get(self):
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    
+    if request.method == 'GET':
         return make_response(jsonify([user.to_dict(rules=('-plans',)) for user in User.query.all()]), 200)
-
-    def post(self):
+    
+    elif request.method == 'POST':
         data = request.get_json()
         user = User(
             name=data.get('name'),
@@ -44,111 +31,124 @@ class UsersEndpoint(Resource):
             age=data.get('age'),
             email=data.get('email'),
             phone=data.get('phone')
-        )
+            )
         db.session.add(user)
-        db.session.commit()
+        db.session.commit() 
 
         user_dict = user.to_dict(rules=('-plans.instructor',))
         return make_response(jsonify(user_dict), 201)
+        # return make_response( jsonify(
+        #     jsonify(
+        #         {'id': user.id, 'name': user.name, 'email': user.email, 'age': user.age }), 201))
 
-# GET, PATCH, DELETE /users/<int:id>
-class UsersByIdEndpoint(Resource):
-    def get(self, id):
-        user = User.query.filter_by(id=id).first()
-        if user is None:
-            return make_response({"error": "User not found."}, 404)
-        return make_response(jsonify(user.to_dict()), 200)
-
-    def patch(self, id):
-        user = User.query.filter_by(id=id).first()
-        if user is None:
-            return make_response({"error": "User not found."}, 404)
-
-        for attr in request.form:
-            setattr(user, attr, request.form.get(attr))
-
-        db.session.add(user)
-        db.session.commit()
-
-        user_dict = user.to_dict()
-        return make_response(user_dict, 200)
-
-    def delete(self, id):
-        user = User.query.filter_by(id=id).first()
-        if user is None:
-            return make_response({"error": "User not found."}, 404)
-
-        db.session.delete(user)
-        db.session.commit()
-
-        response_body = {
-            "delete_successful": True,
-            "message": "User deleted."
+@app.route('/users/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def users_by_id(id):
+    user = User.query.filter_by(id=id).first()
+    
+    if user == None:
+        response_body ={
+            "error!": "User not found."
         }
-
-        return make_response(response_body, 200)
-
-# GET /plans
-class PlansEndpoint(Resource):
-    def get(self):
+        return make_response( response_body, 404 )
+    else: 
+        if request.method == 'GET':
+            user_serializer = user.to_dict()
+            return make_response( jsonify(user_serializer), 200 )
+        
+        elif request.method ==  "PATCH":
+            for attr in request.form:
+                setattr(user, attr, request.form.get(attr))
+            db.session.add(user)
+            db.session.commit()
+            
+            user_dict = user.to_dict()
+            
+            response = make_response(
+                user_dict,
+                200
+            )
+            return response
+        
+        elif request.method == 'DELETE':
+            db.session.delete(user)
+            db.session.commit()
+            
+            response_body = {
+                "delete_successful": True,
+                "message": "User deleted."
+            }
+            
+            response = make_response(
+                response_body,
+                200
+            )
+            
+            return response
+        
+@app.route('/plans', methods= ['GET', 'POST'])
+def plans():
+    if request.method == 'GET':
         return make_response(jsonify([plan.to_dict() for plan in Plan.query.all()]), 200)
-
-    def post(self):
+    
+    elif request.method == 'POST':
         data = request.get_json()
         plan = Plan(
-            user_id=data.get('user_id'),
+            user_id = data.get('user_id'),
             plan_name=data.get('plan_name'),
             package=data.get('package'),
+            )
+        db.session.add(plan)
+        db.session.commit() 
+
+        # plan_dict = new_plan.to_dict(rules=('-plans.instructor',))
+        # return make_response( plan_dict, 201)
+        return make_response(
+            jsonify({'id': plan.id, 'user_id': plan.user_id, 'plan_name': plan.plan_name, 'package':plan.package})
         )
-        db.session.add(plan)
-        db.session.commit()
 
-        return make_response(jsonify({'id': plan.id, 'user_id': plan.user_id, 'plan_name': plan.plan_name, 'package': plan.package}), 201)
+@app.route('/plans/<int:id>', methods= ['GET', 'PATCH'])
+def plan_by_id(id):
+    plan = User.query.filter_by(id=id).first()
+    
+    if plan == None:
+        response_body ={
+            "error!": "User not found."
+        }
+        return make_response( response_body, 404 )
+    else: 
+        if request.method == 'GET':
+            plan_serializer = plan.to_dict()
+            return make_response( jsonify(plan_serializer), 200 )
+        
+        elif request.method ==  "PATCH":
+            for attr in request.form:
+                setattr(plan, attr, request.form.get(attr))
+            db.session.add(plan)
+            db.session.commit()
+            
+            plan_dict = plan.to_dict()
+            
+            response = make_response(
+                plan_dict,
+                200
+            )
+            return response
+            
+@app.route('/plans/plan_name')
+def plans_plan_name():
+    # detail = [pk for pk in Plan.query.order_by(Plan.plan_name).all()]
+    # print(detail) 
+    plans_plan_name = Plan.query.order_by(Plan.plan_name).all()
+    plans_plan_name_serialized = [pn.to_dict() for pn in plans_plan_name]
+    return make_response( jsonify(plans_plan_name_serialized), 200 )
 
-# GET, PATCH /plans/<int:id>
-class PlanByIdEndpoint(Resource):
-    def get(self, id):
-        plan = Plan.query.filter_by(id=id).first()
-        if plan is None:
-            return make_response({"error": "Plan not found."}, 404)
-        return make_response(jsonify(plan.to_dict()), 200)
-
-    def patch(self, id):
-        plan = Plan.query.filter_by(id=id).first()
-        if plan is None:
-            return make_response({"error": "Plan not found."}, 404)
-
-        for attr in request.form:
-            setattr(plan, attr, request.form.get(attr))
-
-        db.session.add(plan)
-        db.session.commit()
-
-        plan_dict = plan.to_dict()
-        return make_response(plan_dict, 200)
-
-# GET /plans/plan_name
-class PlansPlanNameEndpoint(Resource):
-    def get(self):
-        plans_plan_name = Plan.query.order_by(Plan.plan_name).all()
-        plans_plan_name_serialized = [pn.to_dict() for pn in plans_plan_name]
-        return make_response(jsonify(plans_plan_name_serialized), 200)
-
-# GET /instructors
-class InstructorsEndpoint(Resource):
-    def get(self):
+    
+@app.route("/instructors", methods=['GET'])
+def instructor():
+    if request.method == 'GET':
         return make_response(jsonify([instructor.to_dict() for instructor in Instructor.query.all()]), 200)
-
-# Add resources to the API
-api.add_resource(UsersEndpoint, '/users')
-api.add_resource(UsersByIdEndpoint, '/users/<int:id>')
-api.add_resource(PlansEndpoint, '/plans')
-api.add_resource(PlanByIdEndpoint, '/plans/<int:id>')
-api.add_resource(PlansPlanNameEndpoint, '/plans/plan_name')
-api.add_resource(InstructorsEndpoint, '/instructors')
-
+    
+    
 if __name__ == '__main__':
     app.run(port="5555", debug=True)
-
-
-
+    
