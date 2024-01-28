26fdfcc0fe187746208cfa36fd1956c7850642eb
shekinah <nyamburamary1212@gmail.com>
2024-01-28 16:43:05 +0300

    Debug

--- server/app.py
+++ server/app.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
-from flask_cors import CORS  
 from models import db, User, Plan, Instructor
 
-app = Flask(__name__)
-CORS(app) 
+app =Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URI', 'postgresql://user:cCuTqpLrwUFEgOEa0jNiqoCdHQYevka6@dpg-cmpvm0gl5elc73fs5d1g-a.oregon-postgres.render.com/gymdb_a3cw')
